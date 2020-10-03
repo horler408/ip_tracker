c74b920ea30d623902e4a7b1b5688a63d40b90cd
horler408 <horler.web@gmail.com>
2020-10-03 04:05:45 +0100

    Initial UI styling

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //Example Leaflet Map
-/*var map = L.map("map").setView([51.505, -0.09], 13);
+var map = L.map("map").setView([51.505, -0.09], 13);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
@@ -10,11 +10,21 @@ L.marker([51.5, -0.09])
   .addTo(map)
   .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
   .openPopup();
-*/
+
+const searchInput = document.getElementById("search");
+const searchBtn = document.querySelector(".search__button");
+
+let IP_ADD = searchInput.value;
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(IP_ADD);
+  searchInput.value = "";
+});
 
 const API_KEY = "at_7Oso1FsJjRiGPmrL9psu3X9Qe6vHV";
-const IP_ADD = "8.8.8.8";
-const API_URL = "https://geo.ipify.org/api/v1?";
+//const IP_ADD = "8.8.8.8";
+//const API_URL = "https://geo.ipify.org/api/v1?";
 
 const url = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${IP_ADD}`;
 
@@ -24,4 +34,4 @@ const getRequest = async () => {
   console.log(data);
 };
 
-getRequest();
+//getRequest();
