f0ba210f184bcb0ef4a58780e91d6aef27d1633f
horler408 <horler.web@gmail.com>
2020-10-05 18:01:57 +0100

    Working Search Input

--- js/app.js
+++ js/app.js
@@ -1,37 +1,73 @@
-//Example Leaflet Map
-var map = L.map("map").setView([51.505, -0.09], 13);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-  .openPopup();
-
+//Accessing the DOM
 const searchInput = document.getElementById("search");
+const form = document.querySelector(".search");
 const searchBtn = document.querySelector(".search__button");
-
-let IP_ADD = searchInput.value;
-
-searchBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(IP_ADD);
-  searchInput.value = "";
-});
+const errorMsg = document.querySelector(".error-message");
+const ipAddress = document.getElementById("ip-address");
+const timeZone = document.getElementById("time-zone");
+const region = document.getElementById("location");
+const isp = document.getElementById("isp");
+const ispInfo = document.querySelector(".isp__info");
 
 const API_KEY = "at_7Oso1FsJjRiGPmrL9psu3X9Qe6vHV";
-//const IP_ADD = "8.8.8.8";
-//const API_URL = "https://geo.ipify.org/api/v1?";
+let map;
 
-const url = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${IP_ADD}`;
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    getRequest(searchInput.value);
+    searchInput.value = "";
+  });
+}
 
-const getRequest = async () => {
+const defaultRequest = async () => {
+  let url = "https://ipapi.co/json/";
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data.ip);
+  getRequest(data.ip);
+};
+
+const getRequest = async (ip_address) => {
+  let url = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${ip_address}`;
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
+
+  if (ip_address == data.ip) {
+    errorMsg.textContent = `Invalid IP Address!`;
+  }
+
+  //Latitude and Longitude
+  let lat = data.location.lat;
+  let lng = data.location.lng;
+
+  //Details Data
+  ipAddress.textContent = data.ip;
+  timeZone.textContent = data.location.timezone;
+  region.innerHTML = `${data.location.region}, ${data.location.city}<br>${data.location.geonameId}`;
+  isp.textContent = data.isp;
+  ispInfo.innerHTML = `ISP: ${data.as.name}, ${data.as.route}, ${data.as.domain}`;
+
+  // Map
+  renderMap(lat, lng);
 };
 
-//getRequest();
+const renderMap = (lat, lng) => {
+  if (map) {
+    map.remove();
+  }
+  map = L.map("map").setView([lat, lng], 13);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+    .openPopup();
+};
+
+defaultRequest();
